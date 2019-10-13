4464607af2c612768d675d60995ed0d68709a688
benjamin.butn <benjamin.butn@kakaocorp.com>
2019-10-13 16:36:55 +0900

    cur.sh

--- ES-Tutorial-7/tools/curator/cur.sh
+++ ES-Tutorial-7/tools/curator/cur.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-/bin/curator --dry-run --config config/es.config.yml action/es.action.yml # dry-run
-/bin/curator --config config/es.config.yml action/es.action.yml # real
+function delete_indices
+{
+    /bin/curator --dry-run --config config/delete_duration.config.yml action/es.action.yml # dry-run
+    cat logs/curator.log
+    #/bin/curator --config config/es.config.yml action/es.action.yml # real
+}
 
 ##/bin/curator --dry-run --config config/es.config.yml action/alias_es.action.yml
 ##/bin/curator --config config/es.config.yml action/alias_es.action.yml
@@ -23,3 +27,25 @@
 ##
 ##/bin/curator --dry-run --config config/es.config.yml action/warm_es.action.yml
 ##/bin/curator --config config/es.config.yml action/warm_es.action.yml
+
+if [ -z $1 ]; then
+        echo "##################### Menu ##############"
+        echo " $ ./cur.sh [Command]"
+        echo "#####################%%%%%%##############"
+        echo "         1 : delete all indices without kibana index"
+        echo "         2 : configure es hot template"
+        echo "         3 : install elasticdump package"
+        echo "         4 : install telegram package"
+        echo "         5 : install ansible package"
+        echo "#########################################";
+        exit 1;
+fi
+
+case "$1" in
+        "1" ) delete_indices;;
+        "2" ) configure_es_template;;
+        "3" ) install_elasticdump_package;;
+        "4" ) install_telegram_package;;
+        "5" ) install_ansible_package;;
+        *) echo "Incorrect Command" ;;
+esac
